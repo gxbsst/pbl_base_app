39944cd20205d6b098ed14e85e5de721feec263f
liao <aiqing0342@qq.com>
2014-12-10 11:16:05 +0800

    1、modal支持拖动 2、数组项支持软删除

--- app/assets/javascripts/prototype/array.js
+++ app/assets/javascripts/prototype/array.js
@@ -1,17 +1,17 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .extend(Array.prototype, {
-            first: function(){
+            first: function () {
                 return this[0];
             },
-            last: function(){
+            last: function () {
                 return this[this.length - 1];
             },
             index: function (fn) {
                 for (var i = 0; i < this.length; i++) {
-                    if (fn.call(null, this[i])){
+                    if (fn.call(null, this[i])) {
                         return i;
                     }
                 }
@@ -20,7 +20,7 @@
             find: function (fn) {
                 var result = [];
                 for (var i = 0; i < this.length; i++) {
-                    if (fn.call(null, this[i])){
+                    if (fn.call(null, this[i])) {
                         result.push(this[i]);
                     }
                 }
@@ -28,7 +28,7 @@
             },
             findOne: function (fn) {
                 for (var i = 0; i < this.length; i++) {
-                    if (fn.call(null, this[i])){
+                    if (fn.call(null, this[i])) {
                         return this[i];
                     }
                 }
@@ -37,7 +37,16 @@
             has: function (fn) {
                 return !!this.findOne(fn);
             },
-            remove: function (fn) {
+            remove: function (fn, mark) {
+                if (mark) {
+                    var item = this.findOne(fn);
+                    if (typeof item === 'object' && item.id) {
+                        item._destroy = true;
+                        return this;
+                    } else {
+                        return this.remove(fn);
+                    }
+                }
                 var index = this.index(fn);
                 if (index > -1) {
                     this.splice(index, 1);
