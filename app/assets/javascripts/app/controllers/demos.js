f59f9bc8f3f9abc4cae868f1d82f859fe7353951
liao <aiqing0342@qq.com>
2014-12-11 14:49:55 +0800

    1、增加图标 2、增加通用标签添加控件

--- app/assets/javascripts/app/controllers/demos.js
+++ app/assets/javascripts/app/controllers/demos.js
@@ -18,10 +18,6 @@
             ['2:1', '2:2', '2:3'],
             ['3:1', '3:2', '3:3']
         ];
-        vm.project = {
-            tags: ['aa', 'bb', 'cc', 'dd']
-        };
-
         vm.addColumn = function () {
             vm.head.push(['第', vm.head.length + 1, '列'].join(''));
             angular.forEach(vm.body, function (row, i) {
