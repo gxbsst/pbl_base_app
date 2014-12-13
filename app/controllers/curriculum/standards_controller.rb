33ab104e6e2c8c0b0422ed29bcbe4702b4f7f254
liumin <alanliumin@gmail.com>
2014-12-13 13:06:39 +0800

    model名字替换

--- app/controllers/curriculum/standards_controller.rb
+++ app/controllers/curriculum/standards_controller.rb
@@ -2,7 +2,7 @@ module Curriculum
   class StandardsController < ApplicationBaseController
 
     def show
-      @standard = Pbl::Models::Curriculum::Standard.find(params[:id], include_param)
+      @standard = Curriculum::Standard.find(params[:id], include_param)
     end
 
   end
