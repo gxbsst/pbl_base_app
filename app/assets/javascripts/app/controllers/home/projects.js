ad30642957ff32fb936b3773287b8c7dd0198179
liao <aiqing0342@qq.com>
2014-12-14 22:37:57 +0800

    课标选择数据对接

--- app/assets/javascripts/app/controllers/home/projects.js
+++ app/assets/javascripts/app/controllers/home/projects.js
@@ -251,13 +251,80 @@
             //vm.disciplines.push(vm.project.tasks[0].test.discipline);
         });
         vm.cycles=[];
+        //周期未使用异步调用
         vm.cycles=Cycles.all();
 
         vm.selectchange=selectchange;
+        vm.chooseType=chooseType;
+        vm.removeResource=removeResource;
+        vm.addTask=addTask;
+
+
+        vm.onBegin = onBegin;
+        vm.onProgress = onProgress;
+        vm.onSuccess = onSuccess;
+        vm.onCompleted = onCompleted;
+        vm.onError = onError;
+
+        function onBegin(task){
+            return function (files) {
+                console.log(task);
+                console.log(files);
+
+                for(var i = 0 ;i<files.length;i++){
+                    var file=files[i];
+                    task.resources.splice(task.resources.length, 0, {
+                        'id': null,
+                        'title': file.name,
+                        'ext':null,
+                        'state':false
+                    });
+                }
+            }
+        }
+
+        function onProgress(a){
+            return function (b) {
+                console.log(a);
+                console.log(b);
+            }
+        }
+
+        function onSuccess(task){
+            return function (file) {
+                var resource=task.resources.findOne(function(item){
+                    return item.title == file.name;
+                });
+                console.log("onSuccess");
+                console.log(file);
+                resource.id=file.key;
+                resource.ext=file.ext;
+                resource.state=true;
+            }
+        }
+
+        function onCompleted(a){
+            return function (b) {
+                console.log(a);
+                console.log("onCompleted");
+                console.log(b);
+            }
+        }
+
+        function onError(a){
+            return function (b) {
+                console.log(a);
+                console.log(b);
+            }
+        }
 
 
         function selectchange(){
             console.log(vm.project.tasks);
+        }
+
+        function chooseType(task,typeval){
+            task.tasktype=typeval;
         }
 
         function addKnowledge(){
