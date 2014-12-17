0de983b9050fc78dd16767c49da421519c3dade2
liao <aiqing0342@qq.com>
2014-12-17 13:30:18 +0800

    ...

--- app/assets/javascripts/directives/etTags.js
+++ app/assets/javascripts/directives/etTags.js
@@ -5,79 +5,74 @@
         .module('app.directives')
         .directive('etTags', etTags);
 
-    etTags.$inject = ['$timeout'];
+    etTags.$inject = [];
 
-    function etTags($timeout) {
+    function etTags() {
         return {
+            require: 'etTags',
             restrict: 'A',
             replace: true,
-            scope: {
-                ngModel: '=?',
-                placeholder: '@'
-            },
+            scope: true,
             templateUrl: 'directives/et-tags.html',
-            link: etTagsLink
+            link: etTagsLink,
+            controller: angular.noop,
+            controllerAs: 'tagsConfig'
         };
 
-        function etTagsLink(scope, element, attr) {
+        function etTagsLink(scope, element, attr, ctrl) {
 
-            var vm = this,
-                onAdd = scope.$parent.$eval(attr.onAdd) || angular.noop,
-                onRemove = scope.$parent.$eval(attr.onRemove) || angular.noop,
-                onChange = scope.$parent.$eval(attr.onChange) || angular.noop;
+            var onAdd = scope.$eval(attr.onAdd) || angular.noop,
+                onRemove = scope.$eval(attr.onRemove) || angular.noop,
+                onChange = scope.$eval(attr.onChange) || angular.noop;
 
-            scope.$input = '';
-            scope.$tags = [];
-            scope.add = add;
-            scope.remove = remove;
-            scope.onChange = onChange;
-            scope.onKeypress = onKeypress;
-
-            scope.$watch(function () {
-                return scope.ngModel;
-            }, function (tags) {
-                if (tags) {
-                    scope.$tags = tags.split(',');
-                }
+            scope.$watch(attr.ngModel, function (ngModel) {
+                ctrl.ngModel = ngModel;
+                ctrl.tags = ngModel ? ngModel.split(',') : [];
             });
+
+            ctrl.input = '';
+            ctrl.tags = [];
+            ctrl.add = add;
+            ctrl.remove = remove;
+            ctrl.onKeypress = onKeypress;
 
             element.on('click', setFocus);
 
-            function setFocus(){
+            function setFocus() {
                 element.find('.et-tag-input').focus();
             }
 
             function onKeypress($event) {
                 if ($event.which == 13) {
                     $event.preventDefault();
-                    scope.add(scope.$input);
+                    ctrl.add(ctrl.input);
                 }
             }
 
             function add(tag) {
                 tag = tag.trim();
                 if (tag && !exist(tag)) {
-                    scope.$tags.push(tag);
-                    scope.ngModel = scope.$tags.join(',');
+                    ctrl.tags.push(tag);
+                    ctrl.ngModel = ctrl.tags.join(',');
                     setFocus();
-                    onAdd.call(vm, tag, scope.ngModel);
-                    onChange.call(vm, tag, scope.ngModel);
+                    onAdd.call(scope, tag, ctrl.ngModel);
+                    onChange.call(scope, tag, ctrl.ngModel);
                 }
-                scope.$input = '';
+                ctrl.input = '';
             }
 
             function remove(tag, $event) {
                 $event.stopPropagation();
-                scope.ngModel = scope.$tags.remove(function (t) {
+                ctrl.ngModel = ctrl.tags.remove(function (t) {
                     return t == tag;
                 }).join(',');
                 setFocus();
-                onRemove.call(vm, tag, scope.ngModel);
-                onChange.call(vm, tag, scope.ngModel);
+                onRemove.call(scope, tag, ctrl.ngModel);
+                onChange.call(scope, tag, ctrl.ngModel);
             }
 
             function exist(tag) {
-                return scope.$tags.has(function (t) {
+                return ctrl.tags.has(function (t) {
                     return t == tag;
                 });
             }
