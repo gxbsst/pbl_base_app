1101f2f30d9d5e9c9a2a56a06406a940dd0345db
jinxin <aikawa2004@hotmail.com>
2014-12-19 11:19:36 +0800

    project info edit

--- app/assets/javascripts/app/controllers/home/projects/steps/info.js
+++ app/assets/javascripts/app/controllers/home/projects/steps/info.js
@@ -39,6 +39,13 @@
         vm.location=[];
         Location.all(function(data){
             vm.location=data.data;
+            vm.project.location_id=selectisexist(vm.project.location_id,vm.location);
+
+            Projects.update({
+                projectId: vm.project.id
+            }, {
+                project: {location_id:vm.project.location_id}
+            });
         });
 
         console.log(vm.project);
@@ -49,12 +56,20 @@
 
 
         vm.project.duration_unit=selectisexist(vm.project.duration_unit,vm.cycles);
+        vm.project.grade=selectisexist(vm.project.grade,vm.grades);
+
+        Projects.update({
+            projectId: vm.project.id
+        }, {
+            project: {grade:vm.project.grade,duration_unit:vm.project.duration_unit}
+        });
+
 
 
         function selectisexist(obj,array){
             var isexist;
             isexist=array.findOne(function(item){
-                return item.title == obj;
+                return item.id == obj;
             });
             if(isexist===null){
                 return array[0].id;
