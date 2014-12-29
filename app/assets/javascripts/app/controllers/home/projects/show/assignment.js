edbf8fc140b0d6b82064ec9d23462a5301dbb687
jinxin <aikawa2004@hotmail.com>
2014-12-29 19:47:17 +0800

    show assignment

--- app/assets/javascripts/app/controllers/home/projects/show/assignment.js
+++ app/assets/javascripts/app/controllers/home/projects/show/assignment.js
@@ -11,6 +11,18 @@
 
         var vm = this;
         vm.project = project;
+        vm.showTask=showTask;
+        vm.showtask=[];
+        console.log(vm.project);
+
+        vm.showTask(0);
+
+        function showTask(id){
+            //for (var i=0;i<vm.project.tasks.length;i++){
+            //    vm.showtask[i]=false;
+            //}
+            vm.showtask[id]=!vm.showtask[id];
+        }
 
     }
 
