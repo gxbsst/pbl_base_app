707a0c6be87491774aff21a9c1e5fc0f369ebffb
gxbsst <gxbsst@gmail.com>
2015-01-14 19:03:26 +0800

    更改session_store 的保存方式。

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :active_record_store
+#Rails.application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: 'pbl_session'
